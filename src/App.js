9147cbe5fdb1ec06c39c422998d9e5bbaad8909a
Max Long <meax631@gmail.com>
2025-04-03 17:30:12 +0800

    DeepSeek API BE + FE integration complete! Woooooooo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ChatComponent from './components/ChatComponents';
+import PdfUploader from './components/PdfUploader';
+import RenderQA from './components/RenderQA';
+import { Layout, Typography } from "antd";
 
-function App() {
+const chatComponentStyle = {
+  position: "fixed",
+  bottom: "0",
+  width: "80%",
+  left: "10%",
+  marginBottom: "20px"
+};
+
+const pdfUploaderStyle = {
+  margin: "auto",
+  paddingTop: "80px"
+};
+
+const renderQAStyle = {
+  height: "50%",
+  paddingTop: "80px",
+}
+
+// to run both FE and BE concurrently: under root folder-> npm run dev
+const App = () => {
+  const [filePath, setFilePath] = useState(null);
+  const [conversation, setConversation] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { Header, Content } = Layout;
+  const { Title } = Typography;
+
+  const handleResp = (question, answer) => {
+    setConversation([...conversation, { question, answer }]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Layout style={{ height: "100vh", backgroundColor: "white" }}>
+        <Header
+          style={{
+            display: "flex",
+            alignItems: "center"
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Title style={{ color: "white" }}>NeuroPDF</Title>
+        </Header>
+
+        <Content
+          style={{
+            width: "80%",
+            margin: "auto"
+          }}
+        >
+          <div style={{ width: "80%", margin: "auto" }}>
+            <div style={pdfUploaderStyle}>
+              <PdfUploader setFilePath={setFilePath}/>
+            </div>
+          </div>
+
+          <br />
+          <br />
+
+          <div style={renderQAStyle} id='chat-scroll-container'>
+            <RenderQA conversation={conversation} isLoading={isLoading}/>
+          </div>
+
+          <br />
+          <br />
+        </Content>
+          <div style={chatComponentStyle}>
+            <ChatComponent 
+              handleResp={handleResp}
+              isLoading={isLoading}
+              setIsLoading={setIsLoading}
+              filePath={filePath}
+            />
+          </div>
+      </Layout>
+    </>
   );
 }
 
